6f4f1ddc215e5437b28602f582c7b623b082ea6c
Twacqwq <xiaot.qaq@gmail.com>
2025-10-12 10:14:54 +0800

    chore: update example

--- examples/quickstart/main.go
+++ examples/quickstart/main.go
@@ -4,7 +4,10 @@ import "github.com/Twacqwq/mitmfoxy/proxy"
 
 func main() {
 	conf := &proxy.Config{
-		Addr: ":8443",
+		Addr:         ":8443",
+		CertFile:     "../../internal/cert/ca.crt",
+		KeyFile:      "../../internal/cert/ca.key",
+		UseWebsocket: false,
 	}
 
 	mitmfoxy := proxy.New(conf)
